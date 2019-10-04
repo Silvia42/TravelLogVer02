a9458c70f17241117a73a1526bd6f989e78d23bc
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.local>
2019-10-04 16:08:25 -0400

    django server is running

--- travellogApp/serializers.py
+++ travellogApp/serializers.py
@@ -6,12 +6,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User    
         fields = ['id', 'userName', 'email', 'password']
 
-class IssueSerializer(serializers.ModelSerializer):  
+class PlaceSerializer(serializers.ModelSerializer):  
     class Meta:     
         model = Place   
         fields = ['id', 'placeName','description', 'placeImageUrl']
 
-class IssueSerializer(serializers.ModelSerializer):  
+class TripSerializer(serializers.ModelSerializer):  
     class Meta:     
         model = Trip   
         fields = ['id', 'tripDate', 'user', 'place']
