37516dfbecd007ceaf95bf0b9472a46b931766d5
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.local>
2019-10-11 12:42:21 -0400

    select place added

--- client/src/components/Place.js
+++ client/src/components/Place.js
@@ -16,20 +16,24 @@ const placesList = (places) => (
 //   ,description:''
 //  }
 
-const showItem = (x) => {
-return (
-    <div className="oneItem">
-    {/* Name: &nbsp;&nbsp;  */}
-    <a target="_blank" href={x.description}>{x.placeName} </a>
-    {/* <br /> */}
-    {/* Description: {x.description} <br /> */}
-    {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
-    <br />
-    </div>
-    
+const showItem = (x) => (<option value={x.id}>{x.placeName}</option>)
 
-)
-}
+// {
+// return (
+//     // <div className="oneItem">
+//     {/* Name: &nbsp;&nbsp;  */}
+
+//     <option value={x.id}>{x.placeName}</option>
+
+//     {/* <a target="_blank" href={x.description}>{x.placeName} </a> */}
+
+//     {/* <br /> */}
+//     {/* Description: {x.description} <br /> */}
+//     {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
+//     {/* <br /> */}
+//     // </div>
+// )
+// }
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////// class PlaceForm  ////////////////////////////////
@@ -38,10 +42,17 @@ export default class PlaceForm extends React.Component {
   
     render = () => (
         <div>
-            console.log('worldCountries',worldCountries)
-            {/* this.props.match.params.id */}
-            <p>{this.props.worldCountries.map(showItem)}</p> 
             {/* <form onSubmit={this.handleSubmit}> */}
+
+            {/* <select value={0} onChange={(evnt) => console.log(evnt.target.value)}>  */}
+            <select name="place">    
+            <option value ="none">Nothing</option>
+                {this.props.worldCountries.map(showItem)}
+            </select>
+
+                {/* {console.log('worldCountries',this.props.worldCountries)} */}
+                {/* <p>{this.props.worldCountries.map(showItem)}</p>  */}
+                
             {/* </form> */}
             </div>
     )
